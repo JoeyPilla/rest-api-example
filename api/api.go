6439ab9bdfdc8922fc0cf586c6258da15de18525
JoeyPilla <Pilla.4@osu.edu>
2020-05-10 20:43:38 -0400

    added init database

--- api/api.go
+++ api/api.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/lib/pq"
 )
@@ -29,4 +31,35 @@ func Connect() {
 		panic(err)
 	}
 	fmt.Println("Successfully connected!")
+	err = InitalizeDB(db)
+	fmt.Println(err)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully initalized articles table")
+}
+
+func InitalizeDB(db *sql.DB) error {
+	ctx := context.Background()
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = tx.ExecContext(ctx, `CREATE TABLE articles (
+	id integer NOT NULL PRIMARY KEY,
+	content TEXT,
+	authorID integer NOT NULL,
+	title varchar(40) NOT NULL,
+	)`)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return err
 }
